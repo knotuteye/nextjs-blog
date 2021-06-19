94e57e6f8e97718d08c4c7a374fb8348d976700a
Kevin Otuteye <kevinotuteye@gmail.com>
2021-06-19 01:14:16 +0000

    added global _app.js

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,12 +1,17 @@
 import Link from "next/link";
+import Head from "next/head";
+import Layout from "../../components/layout";
 
 export default function FirstPost({}) {
   return (
-    <>
+    <Layout>
+      <Head>
+        <title>First Post</title>
+      </Head>
       <h1>First Post</h1>
       <h3>
         Back to <Link href="/">home</Link>
       </h3>
-    </>
+    </Layout>
   );
 }
