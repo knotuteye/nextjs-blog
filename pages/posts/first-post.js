0883b49443b10480b18a869181cd319fc5ccc371
Kevin Otuteye <kevinotuteye@gmail.com>
2021-06-19 00:38:43 +0000

    added first post

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,5 +1,12 @@
-
-
-export default function FirstPost({ }) {
-    return <h1>First Post</h1>
- }
+import React from "react";
+import Link from "next/Link";
+export default function FirstPost({}) {
+  return (
+    <>
+      <h1>First Post</h1>
+      <h3>
+        Back to <Link href="/">home</Link>
+      </h3>
+    </>
+  );
+}
